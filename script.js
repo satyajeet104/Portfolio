39b529cee334612df1a79d799dcfd697df21f507
Satyajeet Singh <satyajeet104@gmail.com>
2018-01-15 09:26:10 +0530

    Problem with loader resolved

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $(".navbar").hide();
+   
     setTimeout(function(){
 
         $('body').addClass('loaded');
-        $(".navbar").show();
+      
     }, 4000);
  
 });
